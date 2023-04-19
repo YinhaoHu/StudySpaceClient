08f762b0174285be63aba5ac8c1d59ed63a53c0a
HenryHoo <62782257+YinhaoHu@users.noreply.github.com>
2023-04-19 09:34:14 +0800

    debug erase if related probelm

--- lib/CenthooLib/HData/HWData.cpp
+++ lib/CenthooLib/HData/HWData.cpp
@@ -5,18 +5,20 @@
 using namespace ceh::Data;
 using namespace std;
 
-HWData::HWData(const char* filename, wchar_t _delimiter)
-	:delimiter(_delimiter)
+HWData::HWData(const char* _filename, wchar_t _delimiter)
+	:filename(_filename)
 {
-	fileObject.open(filename, ios_base::out | ios_base::in);
-	fileObject.imbue(std::locale(".UTF-8"));
-}
-HWData::~HWData()
-{
-	fileObject.close();
+	in_fileObject.open(filename);
+	in_fileObject.imbue(std::locale(".UTF-8"));
+	delimiter = _delimiter;
 }
 
-HWDataItem& HWData::operator[](int idx)
+HWData::~HWData()
+{
+	
+}
+
+HWDataItem& HWData::operator[](size_t idx)
 {
 	return dataBuffer[idx];
 }
@@ -25,52 +27,106 @@ void HWData::load()
 {
 	wstring lineStr;
 	HWDataItem item;
-
+	
 	dataBuffer.clear();
-	while (getline(fileObject, lineStr,L'\n').good())
+	while (getline(in_fileObject, lineStr,L'\n').good())
 	{
 		item = HWDataItem::fromStdWString(lineStr);
 		dataBuffer.push_back(item);
 	}
-	fileObject.clear();
-	fileObject.seekp(0);
 }
 void HWData::save()
 {
+	in_fileObject.close();
 	size_t loop_end = dataBuffer.size();
+
+	out_fileObject.open(filename, ios::trunc | ios::out);
+
 	for (size_t i = 0; i < loop_end; ++i)
 	{
-		fileObject << HWDataItem::toStdWString(dataBuffer[i]) + L"\n";
+		out_fileObject << HWDataItem::toStdWString(dataBuffer[i]) + L"\n";
 	}
-	fileObject.clear();
-	fileObject.seekg(0);
+	out_fileObject.close();
 }
 
 bool HWData::fail()
 {
-	return fileObject.fail();
+	return in_fileObject.fail();
 }
-int HWData::size()
+
+size_t HWData::size()
 {
 	return dataBuffer.size();
 }
 
 int HWData::find(ceh::Data::HWDataItem& x)
 {
-	for (int idx = 0; idx < dataBuffer.size(); ++idx)
+	for (size_t idx = 0; idx < dataBuffer.size(); ++idx)
 	{
 		if (x == dataBuffer[idx])
 			return idx;
 	}
 	return -1;
 }
-HWDataItem& HWData::access(int idx)
+
+int HWData::find(ceh::Data::HWDataItem&& x)
+{
+	HWDataItem lva = x;
+	return find(lva);
+}
+
+int HWData::findKey(ceh::Data::HWDataItem_key& x) {
+	for (size_t idx = 0; idx < dataBuffer.size(); ++idx)
+	{
+		if (x == dataBuffer[idx].key)
+			return idx;
+	}
+	return -1;
+}
+
+int HWData::findKey(ceh::Data::HWDataItem_key&& x) {
+	HWDataItem_key lva = x;
+	return findKey(lva);
+}
+
+int HWData::findValue(ceh::Data::HWDataItem_value& x, size_t valueIdx) {
+	if (dataBuffer.size() > 0 && dataBuffer[0].values.size() <= valueIdx)
+		throw out_of_range("Out of range");
+	for (size_t idx = 0; idx < dataBuffer.size(); ++idx)
+	{
+		if (x == dataBuffer[idx].values[valueIdx])
+			return idx;
+	}
+	return -1;
+}
+
+int HWData::findValue(ceh::Data::HWDataItem_value&& x, size_t valueIdx) {
+	HWDataItem_value lva = x;
+	return findValue(lva, valueIdx);
+}
+
+int HWData::findValues(ceh::Data::HWDataItem_values& x) {
+	for (size_t idx = 0; idx < dataBuffer.size(); ++idx)
+	{
+		if (x == dataBuffer[idx].values)
+			return idx;
+	}
+	return -1;
+}
+
+int HWData::findValues(ceh::Data::HWDataItem_values&& x) {
+	HWDataItem_values lva = x;
+	return findValues(lva);
+}
+
+HWDataItem& HWData::access(size_t idx)
 {
 	if (idx >= dataBuffer.size())
-		throw out_of_range("[Hdata]Access out of range.\n");
+		throw out_of_range("[HWData]Access out of range.\n");
 	return dataBuffer[idx];
 }
-bool HWData::modify(int idx, ceh::Data::HWDataItem& newItem)
+
+bool HWData::modify(size_t idx, ceh::Data::HWDataItem& newItem)
 {
 	if (idx >= dataBuffer.size())
 		return false;
@@ -80,7 +136,8 @@ bool HWData::modify(int idx, ceh::Data::HWDataItem& newItem)
 		return true;
 	}
 }
-bool HWData::remove(int idx)
+
+bool HWData::remove(size_t idx)
 {
 	if (idx >= dataBuffer.size())
 		return false;
@@ -90,10 +147,12 @@ bool HWData::remove(int idx)
 		return true;
 	}
 }
+
 void HWData::append(ceh::Data::HWDataItem& newItem)
 {
 	dataBuffer.push_back(newItem);
 }
+
 void HWData::append(ceh::Data::HWDataItem&& newItem)
 {
 	dataBuffer.push_back(newItem);
